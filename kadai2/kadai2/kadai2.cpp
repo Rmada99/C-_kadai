c0d1be9877b0f790f31d4e37dcece1fc1790ffcf
XPS\ktc <ktc25a31a0057@edu.kyoto-tech.ac.jp>
2026-05-09 16:25:42 +0900

    課題２途中

---
+++ kadai2/kadai2/kadai2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+//電卓
+int main()
+{
+	printf("電卓\n");
+	printf("式を入力\n");
+
+	float input;
+	scanf_s("%d", &input);
+
+	float answer = input;
+}
